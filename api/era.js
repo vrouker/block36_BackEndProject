a57b8bdbb354d04d8c6acd521e32b6bbc6e73ea2
vrouker <vrouker@gmail.com>
2025-06-05 20:50:35 -0400

    db queries and api calls for eras work!!

--- api/era.js
+++ api/era.js
@@ -1,17 +1,103 @@
 import express from "express";
+import { createEra, deleteEra, getAllEras, getEraById, updateEra } from "../db/queries/era.js";
 const router = express.Router();
 export default router;
 
 //GET /
+    router.route("/").get(async(req, res)=>{
+        const eras = await getAllEras();
+
+        res.send(eras);
+    });
+
+
 
 
 //GET /:id
+    router.route("/:id").get(async(req, res)=>{
+        const id = req.params.id;
+
+        if (!Number.isInteger(id) && id < 0){
+            return res.status(400).send({error: `Please use a valid number for the ID.`})
+        };
+
+        const era = await getEraById(id);
+
+        if (!era){
+            return res.status(404).send({error: `Era not found.`})
+        };
+
+        res.send(era);
+    });
+
+
 
 
 //POST /
+    router.route("/").post(async(req,res)=>{
+        if (!req.body){
+            return res.status(400).send({error: `Please send more details.`})
+        };
+
+        const {name, years_ago} = req.body;
+
+        if (!name || !years_ago){
+            return res.status(400).send({error: `Misisng required params.`})
+        };
+
+        const era = await createEra({name, years_ago});
+
+        res.status(201).send(era);
+    })
+
+
 
 
 //PUT /:id
+    router.route("/:id").put(async(req, res)=>{
+        const id = req.params.id;
+
+
+        if(!req.body){
+            return res.status(400).send({error: `Please send more details.`})
+        };
+
+        const {name, years_ago} = req.body;
+        
+        if (!name || !years_ago){
+            return res.status(400).send({error: `Missing required fields.`})
+        };
+
+        if(!Number.isInteger(id) && id < 0){
+            return res.status(400).send({error: `Please use a valid number for the ID.`})
+        };
+
+        const era = await getEraById(id);
+
+        if (!era){
+            return res.status(404).send({error: `Era not found`})
+        };
+
+        const updatedEra = await updateEra({id, name, years_ago});
+
+        res.send({updatedEra});
+    });
+
 
 
 //DELETE /:id
+    router.route("/:id").delete(async(req,res)=>{
+        const id = req.params.id;
+
+        if (!Number.isInteger(id) && id < 0){
+            return res.status(400).send({error: `Please use a valid number for the ID.`})
+        };
+
+        const deletedEra = await deleteEra(id);
+
+        if (!deletedEra){
+            return res.status(404).send({error: `Era not found.`})
+        };
+
+        res.sendStatus(204);
+    })
--- db/queries/era.js
+++ db/queries/era.js
@@ -14,13 +14,54 @@ export async function createEra({name, years_ago}){
     return era[0]
 };
 
+
+
 //getAllEras()
+export async function getAllEras(){
+    const sql = `
+        SELECT * FROM era;
+    `;
+
+    const {rows: eras} = await db.query(sql);
+    return eras;
+};
+
 
 
 //getEraById(id)
+export async function getEraById(id){
+    const sql = `
+        SELECT * FROM era WHERE id=$1;
+    `;
+
+    const {rows: era} = await db.query(sql, [id]);
+
+    return era[0];
+};
+
 
 
 //updateEra(id, data)
+export async function updateEra({id, name, years_ago}){
+    const sql = `
+        UPDATE era SET name=$1, years_ago=$2 WHERE id=$3 RETURNING *;
+    `;
+
+    const {rows: era} = await db.query(sql, [name, years_ago, id]);
+
+    return era[0];
+};
+
+
 
 
 //deleteEra(id)
+export async function deleteEra(id){
+    const sql = `
+        DELETE FROM era WHERE id=$1 RETURNING *;
+    `;
+
+    const {rows} = await db.query(sql, [id]);
+
+    return rows;
+}
